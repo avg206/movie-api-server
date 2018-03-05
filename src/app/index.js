331a4eeecc1234ce45f3d1a3f37c06efecfbd3da
Anatoli Huseu <avg.tolik@gmail.com>
2018-03-05 16:20:25 +0300

    (move) api to its own folder

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('koa-bodyparser')
 const cors = require('kcors')
 
 const Movies = require('../data/films.json')
-const api = require('./api')
+const api = require('../api')
 
 const app = new Koa()
   .use(cors())
