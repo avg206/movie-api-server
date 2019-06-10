808df04bfe1850db5cd28dc0d405e3101cfd732f
Nikolay Borzov <nikolay_borzov@epam.com>
2019-06-10 16:55:00 +0400

    (fix) updateContext and parseQuery middlewares
    - Koa middle must `return next()` or `await next()`. Otherwise response will be 404

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,14 +2,15 @@ const Movies = require('../data/films.json')
 
 const updateContext = (ctx, next) => {
   ctx.state.Movies = Movies
-  next()
+
+  return next()
 }
 
 const parseQuery = (ctx, next) => {
   ctx.query.offset = (ctx.query.offset && parseInt(ctx.query.offset, 10)) || 0
   ctx.query.limit = (ctx.query.limit && parseInt(ctx.query.limit, 10)) || 10
 
-  next()
+  return next()
 }
 
 module.exports = {
